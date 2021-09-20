4d9f737f77888a7e6b4426fd7d99ade50ba91e32
__atah_atah <ratdotl.hata@gmail.com>
2021-09-21 00:26:19 +0900

    Usage https://pub.dev/packages/easy_localization#usage

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 // https://pub.dev/packages/easy_localization
 // このページの内容を参考に進める。
 
+// 各言語の設定ファイルの変更は Hot Reload ではなく、Hot Restart を使わないと反映されない。
+// また、言語の変更も同様（変数を用いない場合はHot Reloadで反映される）
+
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:easy_localization/easy_localization.dart';
 
 void main() async {
+  // Needs to be called so that we can await for EasyLocalization.ensureInitialized();
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   runApp(EasyLocalization(
@@ -26,6 +30,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // .tr()メソッドなどを使う場合はBuildContextを更新したほうが無難
+    // そうしないと微妙にINFOが出る
     return MaterialApp(
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
@@ -34,15 +40,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -59,21 +63,26 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    const _gender = false;
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('home_title'.tr()),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+              'pushed_times',
+            ).tr(args: ['$_counter']),
+            // args
+            Text('msg').tr(args: ['Easy localization', 'Dart']),
+            // namedArgs
+            Text('msg_named').tr(namedArgs: {'lang': 'Dart'}),
+            // args and namedArgs
+            Text('msg_mixed').tr(args: ['Easy localization'], namedArgs: {'lang': 'Dart'}),
+            // gender
+            Text('gender').tr(gender: _gender ? "female" : "male", args: ['西']),
           ],
         ),
       ),
